56d12b8b4c4a6e750bebf7552d5ed596b3086d0e
aster <whirlwindaster@gmail.com>
2025-12-04 03:21:38 -0500

    day 4, bump version

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,71 @@
+use std::cmp::min;
+
 pub trait NumDigits {
-    fn num_digits(&self) -> Self;
+    fn num_digits(&self) -> u8;
 }
 
 impl NumDigits for u64 {
-    fn num_digits(&self) -> Self {
-        (self.ilog10() + 1) as u64
+    fn num_digits(&self) -> u8 {
+        (self.ilog10() + 1) as u8
+    }
+}
+
+pub fn num_adjacent<T, P>(row: usize, col: usize, predicate: P, grid: &[Vec<T>]) -> usize
+where
+    P: Fn(&T) -> bool,
+{
+    grid[row.saturating_sub(1)..=min(row + 1, grid.len() - 1)]
+        .iter()
+        .map(|tiles| {
+            tiles[col.saturating_sub(1)..=min(col + 1, grid.len() - 1)]
+                .iter()
+                .filter(|&t| predicate(t))
+                .count()
+        })
+        .sum::<usize>()
+        - if predicate(&grid[row][col]) { 1 } else { 0 }
+}
+
+pub struct IterAdjacent {
+    init_row: usize,
+    row: usize,
+    row_bound: usize,
+    init_col: usize,
+    col: usize,
+    col_bound: usize,
+}
+
+impl Iterator for IterAdjacent {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.row == self.row_bound {
+            None
+        } else if self.col == self.col_bound {
+            self.col = self.init_row.saturating_sub(1);
+            self.row += 1;
+            self.next()
+        } else if self.row == self.init_row && self.col == self.init_col {
+            self.col += 1;
+            self.next()
+        } else {
+            self.col += 1;
+            Some((self.row, self.col - 1))
+        }
+    }
+
+    // TODO make this exact size
+}
+
+impl IterAdjacent {
+    pub fn new(init_row: usize, init_col: usize, row_bound: usize, col_bound: usize) -> Self {
+        IterAdjacent {
+            init_row,
+            row: init_row.saturating_sub(1),
+            row_bound,
+            init_col,
+            col: init_col.saturating_sub(1),
+            col_bound,
+        }
     }
 }
--- src/year2025/day4.rs
+++ src/year2025/day4.rs
@@ -1,3 +1,7 @@
+use aoc::{IterAdjacent, num_adjacent};
+
+const MAX_ADJACENT: usize = 4;
+
 pub fn run(part: Option<u8>, input: &str) {
     match part {
         Some(p) => {
@@ -14,10 +18,56 @@ pub fn run(part: Option<u8>, input: &str) {
     }
 }
 
-fn part_one(_: &str) {
-    todo!();
+fn part_one(input: &str) {
+    let grid = parse_grid(input);
+    println!(
+        "{}",
+        grid.iter()
+            .enumerate()
+            .map(|(row, tiles)| tiles
+                .iter()
+                .enumerate()
+                .filter(|&(col, is_roll)| {
+                    *is_roll && num_adjacent(row, col, |t| *t, &grid) < MAX_ADJACENT
+                })
+                .count())
+            .sum::<usize>()
+    );
 }
 
-fn part_two(_: &str) {
-    todo!();
+fn part_two(input: &str) {
+    let mut grid = parse_grid(input);
+    let rows = grid.len();
+    let cols = grid[0].len();
+    let mut sum = 0;
+
+    for row in 0..rows {
+        for col in 0..cols {
+            if grid[row][col] && num_adjacent(row, col, |t| *t, &grid) < MAX_ADJACENT {
+                sum += chain_removals(row, col, &mut grid)
+            }
+        }
+    }
+
+    println!("{sum}");
+}
+
+fn chain_removals(row: usize, col: usize, grid: &mut [Vec<bool>]) -> usize {
+    if !grid[row][col] {
+        0
+    } else {
+        grid[row][col] = false;
+        1 + IterAdjacent::new(row, col, grid.len(), grid[0].len())
+            .filter_map(|(r, c)| {
+                (grid[r][c] && num_adjacent(r, c, |t| *t, grid) < MAX_ADJACENT)
+                    .then(|| chain_removals(r, c, grid))
+            })
+            .sum::<usize>()
+    }
+}
+
+fn parse_grid(grid: &str) -> Vec<Vec<bool>> {
+    grid.lines()
+        .map(|row| row.chars().map(|ch| ch == '@').collect::<Vec<bool>>())
+        .collect::<Vec<Vec<bool>>>()
 }
