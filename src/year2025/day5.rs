439b6a40c177055e6e7222991e00fcaedb6ba45a
aster <whirlwindaster@gmail.com>
2025-12-05 03:36:10 -0500

    d5p1

--- src/year2025/day5.rs
+++ src/year2025/day5.rs
@@ -1,8 +1,29 @@
 pub struct Solution;
 
 impl crate::Solution for Solution {
-    fn part_one(_input: &str) {
-        todo!();
+    fn part_one(input: &str) {
+        let mut ranges = Vec::new();
+
+        let mut lines = input.lines();
+        while let Some(line) = lines.next() {
+            if line.is_empty() {
+                break;
+            }
+
+            let range = line
+                .split('-')
+                .map(|id| id.parse::<u64>().unwrap())
+                .collect::<Vec<_>>();
+            ranges.push((range[0], range[1]));
+        }
+
+        println!(
+            "{}",
+            lines
+                .map(|id| id.parse::<u64>().unwrap())
+                .filter(|id| ranges.iter().any(|(begin, end)| id >= begin && id <= end))
+                .count()
+        );
     }
 
     fn part_two(_input: &str) {
